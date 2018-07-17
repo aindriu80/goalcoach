e03b90ba5b689a2013488a14537128b5445fddfa
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-07-17 22:11:31 +0100

    Adding Firebase to App

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+const config =
+{
+    apiKey: "AIzaSyB5VAxoa9kCRxvMv_Axe6b2pq0g8pvk9OM",
+    authDomain: "goalcoach-3c675.firebaseapp.com",
+    databaseURL: "https://goalcoach-3c675.firebaseio.com",
+    projectId: "goalcoach-3c675",
+    storageBucket: "",
+    messagingSenderId: "777027455056"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
