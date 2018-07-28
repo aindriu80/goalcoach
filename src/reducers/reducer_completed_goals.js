e8522415a0e06e15d64d3766f636728733e942cc
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-07-28 16:56:00 +0100

    Rendering Completed Goals

--- src/reducers/reducer_completed_goals.js
+++ src/reducers/reducer_completed_goals.js
@@ -3,8 +3,8 @@ import { SET_COMPLETED } from '../constants';
 export default (state = [], action) => {
     switch (action.type) {
         case SET_COMPLETED:
-            const { completedGoals } = action;
-            return completedGoals;
+            const { completeGoals } = action;
+            return completeGoals;
         default:
             return state;
     }
